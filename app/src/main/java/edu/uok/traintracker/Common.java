c7523fe76da91cde7ec31fe37a714a88c7d8cbc1
mohammeda <moh999ah@gmail.com>
2021-04-05 10:00:43 +0200

    Presence + Sign out

--- app/src/main/java/edu/uok/traintracker/Common.java
+++ app/src/main/java/edu/uok/traintracker/Common.java
@@ -7,4 +7,15 @@ public class Common {
     public static final String DRIVERS_LOCATION_REFERENCES = "DriverLocation";
 
     public static DriverInfoModel currentUser;
+
+    public static String buildWelcomeMessage() {
+        if (Common.currentUser != null) {
+            return new StringBuilder("Welcome ")
+                    .append(Common.currentUser.getFirstName())
+                    .append(" ")
+                    .append(Common.currentUser.getLastName()
+                    ).toString();
+        } else
+            return "";
+    }
 }
--- app/src/main/java/edu/uok/traintracker/DriverHomeActivity.java
+++ app/src/main/java/edu/uok/traintracker/DriverHomeActivity.java
@@ -1,13 +1,20 @@
 package edu.uok.traintracker;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.Menu;
+import android.widget.TextView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
 
+import androidx.annotation.NonNull;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -20,6 +27,10 @@ public class DriverHomeActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
 
+    private DrawerLayout drawer;
+    private NavigationView navigationView;
+    private NavController navController;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,17 +38,60 @@ public class DriverHomeActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
-        NavigationView navigationView = findViewById(R.id.nav_view);
+        drawer = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
+                R.id.nav_home)
                 .setDrawerLayout(drawer)
                 .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if (item.getItemId() == R.id.nav_sign_out) {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(DriverHomeActivity.this);
+                    builder.setTitle("Sign Out!")
+                            .setMessage("Do you really want to sign out?")
+                            .setNegativeButton("CANCEL", (dialog, which) -> dialog.dismiss())
+                            .setPositiveButton("SIGN OUT", (dialog, which) -> {
+                                FirebaseAuth.getInstance().signOut();
+                                Intent intent = new Intent(DriverHomeActivity.this, SplashScreenActivity.class);
+                                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                                startActivity(intent);
+                                finish();
+                            }).setCancelable(false);
+                    AlertDialog dialog = builder.create();
+                    dialog.setOnShowListener(dialog1 -> {
+                        dialog.getButton(AlertDialog.BUTTON_POSITIVE)
+                                .setTextColor(getResources().getColor(android.R.color.holo_red_dark));
+                        dialog.getButton(AlertDialog.BUTTON_NEGATIVE)
+                                .setTextColor(getResources().getColor(R.color.colorAccent));
+                    });
+
+                    dialog.show();
+                }
+
+
+                return true;
+            }
+        });
+
+
+        //set data for user
+        View headerView = navigationView.getHeaderView(0);
+        TextView textName = (TextView) headerView.findViewById(R.id.textName);
+        TextView textPhone = (TextView) headerView.findViewById(R.id.textPhone);
+        TextView textStar = (TextView) headerView.findViewById(R.id.textStar);
+
+        textName.setText(Common.buildWelcomeMessage());
+        textPhone.setText(Common.currentUser != null ? Common.currentUser.getPhoneNumber() : "");
+        textStar.setText(Common.currentUser != null ? String.valueOf(Common.currentUser.getRating()) : "0.0");
+
+
     }
 
     @Override
